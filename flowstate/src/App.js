dde26dc310478e6443d6779ee2e6994a3f002091
Fuad Farhan <fuadhoquef8414@hstat.org>
2025-10-27 13:20:24 -0400

    styling and background

--- flowstate/src/App.js
+++ flowstate/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState, useRef } from 'react';
 import { FaArrowUp } from "react-icons/fa";
-import LiquidEther from './background';
+import Particles from './backgrounds/particles';
 
 function App() {
   const [upload, setUpload] = useState(null);
@@ -42,31 +42,40 @@ function App() {
 
   return (
     <div className="App">
-      {/* LiquidEther background */}
-      <div className="liquid-ether-container">
-        <LiquidEther
-          style={{ width: '100%', height: '100%' }}  // important!
-          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.5}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
-        />
-      </div>
+
+  
 
       <header className="App-header">
-        <h1>FlowState</h1>
+        
 
+                        <div
+                style={{
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  width: '100vw',
+                  height: '100vh',
+                  zIndex: 0,
+                  pointerEvents: 'none'
+                }}
+              >
+                <Particles
+                  particleColors={['#c861fcff', '#c383f4ff']}
+                  particleCount={800}
+                  particleSpread={15}
+                  speed={0.2}
+                  particleBaseSize={450}
+                  moveParticlesOnHover={true}
+                  alphaParticles={false}
+                  disableRotation={false}
+                />
+              </div>
+
+
+
+
+        <h1>FlowState</h1>
+      
         <div className="wrapper">
           <input type="text" className="input" placeholder="Type here..." />
           <button className="button" onClick={handleSubmit}><FaArrowUp /></button>
