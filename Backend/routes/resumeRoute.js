010d818bb3ff9d5b7ff9714e39a5a26c7ae5069e
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-10-29 21:45:45 -0400

    adding to do lists researching is beginning

--- Backend/routes/resumeRoute.js
+++ Backend/routes/resumeRoute.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const resumeRoute = express.Router(); 
 
+//TODO: change route to a post request since we are uploading from client side as well. 
 resumeRoute.get('/Resume', (req, res) => { 
     res.send({ 
         ResumeEndpoint: true
