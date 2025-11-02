e1ebcf6c43d89b25fbbaf76cdf97949b406383b9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-02 15:22:55 -0500

    used prettier to write the code better

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,25 @@
-//Instantiate an Express App for backend.  
-// this will be the main server file here as such 
-const express = require('express'); 
-const app = express(); 
+//Instantiate an Express App for backend.
+// this will be the main server file here as such
+const express = require("express");
+const app = express();
 
+//Importing routes here as such
+const ResumeRoute = require("./routes/resumeRoute.js");
 
-//Importing routes here as such 
-const ResumeRoute = require('./routes/resumeRoute.js'); 
+//basic middleware
+app.use(express.json());
 
-//basic middleware 
-app.use(express.json());  
+const PORT = 3500 || 6500;
 
-const PORT = 3500 || 6500; 
+app.get("/", (req, res) => {
+  res.send({
+    BackendRunning: true,
+  });
+});
 
-app.get('/', (req, res) => { 
-    res.send({ 
-        BackendRunning: true
-    }); 
-});  
+//calling the routes here as such
+app.use("/api/v1/", ResumeRoute);
 
-//calling the routes here as such 
-app.use('/api/v1/', ResumeRoute); 
-
-app.listen(PORT, () => { 
-    console.log(`Server running on http://localhost:${PORT}`); 
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
