1771908a93d5166b7b2081b6c6d5ea7515400376
Fuad Farhan <fuadhoquef8414@hstat.org>
2025-12-02 19:07:18 -0500

    preview design fix

--- flowstate/src/pages/analysishome.js
+++ flowstate/src/pages/analysishome.js
@@ -5,6 +5,11 @@ function AnalysisPreview() {
     <div className="App">
       <div>
         <h2 style={{ color: 'white' }}>Let's dive in!</h2>
+        <div class="page">
+          <div class="page-content">
+            <h2>Content</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
