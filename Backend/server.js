920ee2dd81404cca62827a37d53ac72d07ab91d4
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-08 18:10:04 -0500

    hard coded PORT so you are able to run it

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 //call db connection later on in the server.js file JUST the connection   
 
 const app = require("./app.js"); 
-const PORT = process.env.PORT || 6000; 
+const PORT = 5434; 
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
