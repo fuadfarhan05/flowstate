449d3fadbf6cce8bcdc2bdaa66a03fd1a84c6e05
Fuad Farhan <fuadhoquefarhan05@gmail.com>
2026-01-15 21:52:00 -0500

    restarting speech to text functionality

--- flowstate/src/pages/interviewsim.js
+++ flowstate/src/pages/interviewsim.js
@@ -1,66 +1,7 @@
-import CameraPreview from "../components/CameraMic";
-import useContinuousSpeech from "../components/speechtotext";
-import {useState} from 'react'
-import Aurora from "../backgrounds/auroura";
+import { useState } from 'react'
 import { useLocation } from "react-router-dom";
 
 function InterviewSimulation() {
-
-  const location = useLocation();
-  const experienceTitle = location.state?.experienceTitle;
-  
-  const [count, setCount] = useState(0);
-  const questionList = [
-  `Tell me about your experience at ${experienceTitle}`,
-  "What challenges did you face during this experience?",
-  "What impactful contributions did you make in this experience?"
-];
-
-  
-  const { transcript } = useContinuousSpeech();
-
-  const questionrender =
-  count < questionList.length
-    ? questionList[count]
-    : "You're Done";
-
-  function nextQuestion() {
-    if (count < questionList.length) {
-      gradeAnswer(count);
-      setCount(prev => prev + 1);
-    }
-  }
-
-
-  const gradeAnswer = async () => {
-      const question_and_answers = {
-      qa_pairs: [
-        {
-          question: questionList[count],
-          answer: transcript
-        }
-      ]
-    };
-      try {
-        const res = await fetch("http://localhost:5434/api/v1/grade-answers", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(question_and_answers)
-
-      });
-
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
-      }
-
-      const result = await res.json();
-      console.log(result);
-
-
-      } catch(error) {
-        console.log("failed to retrieve your answer", error);
-      }
-  }
 
   return (
     <div
@@ -105,13 +46,11 @@ function InterviewSimulation() {
             marginTop: "-15px",
           }}
         >
-          Q: {questionrender}
+          Q:
         </h2>
 
-        <CameraPreview />
-        
-        <p style={{color:'white'}}>{transcript}</p>
-        <button className="next-button" onClick={nextQuestion}>Next Question</button>
+  
+        <button className="next-button">Next Question</button>
       </div>
       
     </div>
