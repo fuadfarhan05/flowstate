dc8f16ad438579a5ffcd1e4ae2677452ccbcb449
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-18 13:28:48 -0500

    deleted work of my branch

--- flowstate/src/App.js
+++ flowstate/src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/home';
-// import AnalysisPreview from './pages/analysishome';
-// import InterviewSimulation from './pages/interviewsim'; 
-import Homepage from './pages/raysdesign.ui'; 
+import Home from './pages/home';
+import AnalysisPreview from './pages/analysishome';
+import InterviewSimulation from './pages/interviewsim'; 
 
 function App() {
   return (
     <div className="App">
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        {/* <Route path="/analysis" element={<AnalysisPreview />} />
-        <Route path="/interview" element={<InterviewSimulation/>} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/analysis" element={<AnalysisPreview />} />
+        <Route path="/interview" element={<InterviewSimulation/>} />
       </Routes>
     </Router>
    </div>
--- flowstate/src/pages/interviewsim.js
+++ flowstate/src/pages/interviewsim.js
@@ -1,60 +1,60 @@
-// import { useState } from 'react'
-// import { useLocation } from "react-router-dom";
-// import ElevenLabs from '../components/elevenlabcomp';
+import { useState } from 'react'
+import { useLocation } from "react-router-dom";
+import ElevenLabs from '../components/elevenlabcomp';
 
-// function InterviewSimulation() {
+function InterviewSimulation() {
 
-//   return (
-//     <div
-//       style={{
-//         position: "relative",
-//         minHeight: "100vh",
-//         overflow: "hidden",
-//         backgroundColor: "#05080d"
-//       }}
-//     >
-//       {/* 🌌 Background Aurora */}
+  return (
+    <div
+      style={{
+        position: "relative",
+        minHeight: "100vh",
+        overflow: "hidden",
+        backgroundColor: "#05080d"
+      }}
+    >
+      {/* 🌌 Background Aurora */}
       
 
-//       {/* 🔝 Foreground Content */}
-//       <div style={{ position: "relative", zIndex: 2 }}>
-//         <div
-//           style={{
-//             display: "flex",
-//             alignItems: "center",
-//             position: "relative",
-//             left: "30px",
-//           }}
-//         >
-//           <h3 style={{ color: "white", fontSize: "25px", margin: 0 }}>
-//             FlowState
-//           </h3>
-//           <h3
-//             style={{
-//               color: "#80e1f9",
-//               fontSize: "25px",
-//               marginLeft: "6px",
-//             }}
-//           >
-//             Interviews
-//           </h3>
-//         </div>
-//         <h2
-//           style={{
-//             color: "white",
-//             fontSize: "30px",
-//             textAlign: "center",
-//             marginTop: "-15px",
-//           }}
-//         >
-//           Q:
-//         </h2>
+      {/* 🔝 Foreground Content */}
+      <div style={{ position: "relative", zIndex: 2 }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            position: "relative",
+            left: "30px",
+          }}
+        >
+          <h3 style={{ color: "white", fontSize: "25px", margin: 0 }}>
+            FlowState
+          </h3>
+          <h3
+            style={{
+              color: "#80e1f9",
+              fontSize: "25px",
+              marginLeft: "6px",
+            }}
+          >
+            Interviews
+          </h3>
+        </div>
+        <h2
+          style={{
+            color: "white",
+            fontSize: "30px",
+            textAlign: "center",
+            marginTop: "-15px",
+          }}
+        >
+          Q:
+        </h2>
 
-//         <ElevenLabs/>
-//       </div>
+        <ElevenLabs/>
+      </div>
       
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default InterviewSimulation;
+export default InterviewSimulation;
