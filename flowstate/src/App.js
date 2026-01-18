0abbb83152165b74c41ffcf055b3ec37031aa288
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-18 13:00:23 -0500

    weird file null

--- flowstate/src/App.js
+++ flowstate/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import AnalysisPreview from './pages/analysishome';
-import InterviewSimulation from './pages/interviewsim';
+// import Home from './pages/home';
+// import AnalysisPreview from './pages/analysishome';
+// import InterviewSimulation from './pages/interviewsim'; 
+import Homepage from './pages/raysdesign.ui'; 
 
 function App() {
   return (
     <div className="App">
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/analysis" element={<AnalysisPreview />} />
-        <Route path="/interview" element={<InterviewSimulation/>} />
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/analysis" element={<AnalysisPreview />} />
+        <Route path="/interview" element={<InterviewSimulation/>} /> */}
       </Routes>
     </Router>
    </div>
