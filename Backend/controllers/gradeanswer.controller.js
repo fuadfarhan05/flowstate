5a23e1a81eae92643f1e1e689ef75734e56c69e8
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-22 21:04:49 -0500

    finished the openAi route

--- Backend/controllers/gradeanswer.controller.js
+++ Backend/controllers/gradeanswer.controller.js
@@ -1,20 +1,40 @@
+const OpenAi = require('openai'); 
+const client = new OpenAi({ 
+    apiKey: process.env.OPENAIAPIKEY
+}); 
+
 const GradeAnswer = async (req, res) => {
   //for now route just takes in response.
   try {
     const { question, answer } = req.body;
-    if (
-      !question ||
-      question.trim() === "" ||
-      !answer ||
-      answer.trim() === ""
-    ) {
-      return res
-        .status(400)
-        .json({ error: "both question and answer are required for grade" });
-    }
+        if (!question || question.trim() === "" || !answer || answer.trim() === "") {
+            return res.status(400).json(
+                { 
+                    error: "both question and answer are required for grade" 
+                }
+            );
+        }
+    console.log("recieved, ready to grade"); 
+    console.log("sending grade to the backend now"); 
 
-    console.log("recieved, ready to grade");
-    return res.status(200).send();
+    const AIresponse = await client.chat.completions.create({  
+        model: 'gpt-4o-mini',
+        messages: [
+            { 
+            role: 'system',
+            content: 'Fuad enter research here about prompt'
+            }, 
+            { 
+                role: 'user', 
+                content: `Question asked: ${question}\n\n Answer Given: ${answer}` 
+            }
+        ] 
+    }); 
+    const AiEvaluation = { evaluation: AIresponse.choices[0].message.content }; // in the frontend remember to call the Evaluation key to get the AI data to show in the frontend.  
+    console.log(AiEvaluation); 
+    return res.status(200).json({ 
+        AiEvaluation // remember this is already an object. 
+    }); 
   } catch (error) {
     console.log("error with grade route", error);
     return res.status(500).json({ error: "grading failed" });
