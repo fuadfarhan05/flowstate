30045e2fc5528ecdf6f25ac523e3f02f3455b8df
Fuad Farhan <fuadhoquefarhan05@gmail.com>
2026-02-21 11:26:14 -0500

    fixed routing

--- flowstate/src/App.js
+++ flowstate/src/App.js
@@ -2,7 +2,6 @@ import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import AnalysisPreview from "./pages/analysishome";
-import InterviewSimulation from "./pages/interviewsim";
 import Landing from "./pages/landingpage";
 import GradeArchives from "./pages/gradearchives";
 import CreatePage from "./pages/createpage";
@@ -11,18 +10,21 @@ import ElevenLabs from "./components/elevenlabcomp";
 import Results from "./pages/resultspage";
 import LoginPage from "./pages/loginpage";
 import SignUpPage from "./pages/signuppage";
-import AccessPage from "./pages/accessform";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={< AccessPage />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/analysis" element={<AnalysisPreview />} />
           <Route path="/interview" element={<ElevenLabs />} />
+          <Route path="/archives" element={<GradeArchives />} />
           <Route path="/results" element={<Results />} />
-          
-
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/loginpage" element={<LoginPage />} />
+          <Route path="/signuppage" element={<SignUpPage />} />
         </Routes>
       </Router>
     </div>
