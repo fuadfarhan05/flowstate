ed9774cc23cd010d2e12c1c68fd802e01a387660
Fuad Farhan <fuadhoquefarhan05@gmail.com>
2026-03-09 22:20:22 -0400

    eleven and assembly

--- Backend/app.js
+++ Backend/app.js
@@ -46,6 +46,7 @@ const googleOauth = require("./routes/googleoauth.route.js");
 const ExperienceQuestionRoute = require("./routes/experienceq.route.js");
 const AssemblyRoute = require("./routes/assembly.route.js");
 const AccessRoute = require("./routes/access.route.js");
+const ElevenLabsRoute = require("./routes/scribeToken.route.js");
 
 app.get("/", (req, res) => {
   res.json({
@@ -61,6 +62,7 @@ app.use("/api/v1/", GradeAnswerRoute);
 app.use("/api/v1/", ExperienceQuestionRoute);
 app.use("/api/v1/", userRoute);
 app.use("/api/v1/", AssemblyRoute);
+app.use("/api/v1/", ElevenLabsRoute);
 app.use("/api/v1/", AccessRoute);
 app.use("/", googleOauth);
 
